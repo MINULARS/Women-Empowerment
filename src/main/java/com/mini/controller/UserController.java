73300de6562e34dbc0bce6b6013580435ca674b8
Minu Mishra <minutanoch@gmail.com>
2022-03-20 14:53:32 +0530

    lets empower again

--- src/main/java/com/mini/controller/UserController.java
+++ src/main/java/com/mini/controller/UserController.java
@@ -17,7 +17,7 @@ import com.mini.entity.User;
 import com.mini.service.UserService;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins="http://localhost:4200/")
 public class UserController {
 
 	@Autowired
